2b8fee9a748972b958e70a55276bd46927f04723
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2026-02-07 16:56:53 +0200

    Created class User.py. Enjoy ;)

---
+++ app/models/User.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field, EmailStr
+from typing import Optional
+
+class User(BaseModel):
+    # [Key] - Optional, защото в началото нямаме ID (преди записа в БД)
+    id: Optional[int] = None
+
+    # [Required], [StringLength(100, MinimumLength=3)]
+    username: str = Field(..., min_length=3, max_length=100)
+
+    # [Required], [EmailAddress]
+    email: EmailStr
+
+    # [Required], [StringLength(100)]
+    first_name: str = Field(..., max_length=100)
+
+    # [Required], [StringLength(100)]
+    last_name: str = Field(..., max_length=100)
+
+    # [Required], [Range(1, 120)]
+    age: int = Field(..., gt=0, lt=120)
+
+    # Пример за изчислено свойство (като Read-only Property в C#)
+    def get_full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+    
+test = User(username="Koki", email="koki@abv.bg", first_name= "Kaloyan", last_name= "Rusev", age=19 )
+print(test.get_full_name());
