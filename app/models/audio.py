8deeca9ef67eb15a5f777a11ebccfa780ec98902
Kokimoko4a <106096547+Kokimoko4a@users.noreply.github.com>
2026-02-09 14:43:55 +0200

    The class Audio is ready. Enjoy ;)

--- app/models/audio.py
+++ app/models/audio.py
@@ -1,2 +1,16 @@
-class Audio:
-    name: int
+from pydantic import BaseModel, Field, EmailStr
+from typing import Optional
+from datetime import datetime
+from User import User
+
+class Audio(BaseModel):
+
+    id: Optional[int] = None
+
+    user: User
+
+    file_name: str = Field(..., max_length= 255)
+
+    file_path: str
+
+    created_at: datetime = Field(default_factory=datetime.now)
